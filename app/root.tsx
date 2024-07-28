79a0d025eca2c1c926dd52173f33074869cb4d14
GabrielCardoz0 <gabriel.scardozo7@gmail.com>
2024-07-28 09:12:16 -0300

    feat: initilize and config project

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,18 @@
 import {
   Links,
+  LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
 import "./tailwind.css";
+import { LinksFunction } from "@remix-run/node";
+import styles from "./index.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -25,6 +32,22 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
+
 export default function App() {
-  return <Outlet />;
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload  />
+      </body>
+    </html>
+  );
 }
